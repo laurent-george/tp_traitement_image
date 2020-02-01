e5fb823af1fff48d770fa2c3b84e94c3e51fb53f
Laurent George <laurent.f.george@gmail.com>
2020-02-01 09:47:37 +0100

    Update conda version

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash 
 
-wget https://repo.continuum.io/archive/Anaconda3-2018.12-Linux-x86_64.sh
-chmod +x ./Anaconda3-2018.12-Linux-x86_64.sh
-./Anaconda3-2018.12-Linux-x86_64.sh -f -b
+rm -Rf ~/anaconda3
+wget https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
+chmod +x Anaconda3-2019.10-Linux-x86_64.sh
+bash Anaconda3-2019.10-Linux-x86_64.sh -f -b
 
 echo ". ~/anaconda3/etc/profile.d/conda.sh" >> ~/.bashrc
 source ~/anaconda3/etc/profile.d/conda.sh
